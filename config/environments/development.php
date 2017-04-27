c7542ea4d80c09076c5c05bb99601abc2072d1d9
Troy McGinnis <hello@troymcginnis.com>
2017-04-27 14:07:07 -0700

    Update development.php

--- config/environments/development.php
+++ config/environments/development.php
@@ -8,6 +8,6 @@ define('JETPACK_DEV_DEBUG', true);
 /**
  * Mailgun settings
  */
-define('MAILGUN_USEAPI', true);
-define('MAILGUN_APIKEY', 'key-fae041203f6618ee3655bcd940da2e0f');
-define('MAILGUN_DOMAIN', 'sandbox36b783bae48e4443b4423487a73d4a2d.mailgun.org');
+// define('MAILGUN_USEAPI', true);
+// define('MAILGUN_APIKEY', 'key-fae041203f6618ee3655bcd940da2e0f');
+// define('MAILGUN_DOMAIN', 'sandbox36b783bae48e4443b4423487a73d4a2d.mailgun.org');
